4ea3182123c2c3dfea679749175ec61b263991b0
Selvaraj, Gopalakrishnan <gopalofssn@gmail.com>
2021-03-07 14:59:58 -0500

    draft
    draft

---
+++ src/main/java/com/gs/linkedlist/buffer/LimitedCapacityBuffer.java
@@ -0,0 +1,12 @@
+package com.gs.linkedlist.buffer;
+
+public class LimitedCapacityBuffer {
+
+  private final int capacity;
+
+  public LimitedCapacityBuffer(int capacity) {
+      this.capacity = capacity;
+  }
+
+  
+}
---
+++ src/main/java/com/gs/linkedlist/node/Node.java
@@ -0,0 +1,10 @@
+package com.gs.linkedlist.node;
+
+public class Node<T> {
+  private T value;
+  private Node<T> node;
+
+  public Node(T value) {
+    this.value = value;
+  }
+}
